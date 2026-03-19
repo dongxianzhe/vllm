a45591bd99e4f9b5cceb752faaac6eef048e7e4d
xianzhedong <xianzhedong@tencent.com>
2026-03-19 16:39:32 +0800

    feat: download model and dataset.

---
+++ playground/download_dataset.sh
@@ -0,0 +1,2 @@
+mkdir -p /mnt/cephfs/user_xianzhedong/datasets
+wget https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen3-Omni/cookbook/caption2.mp3 -o /mnt/cephfs/user_xianzhedong/datasets/caption2.mp3
---
+++ playground/download_model.sh
@@ -0,0 +1,2 @@
+mkdir -p /mnt/cephfs/user_xianzhedong/models
+hf download Qwen/Qwen3-Omni-30B-A3B-Captioner --local-dir /mnt/cephfs/user_xianzhedong/models/Qwen3-Omni-30B-A3B-Captioner
